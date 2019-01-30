ce7e6aeb7690535a35015e3fafda4c687454d41a
guccang <guccang@126.com>
2019-01-30 16:38:41 +0800

    crystal

---
+++ crystal/Assets/Scripts/UI/HexGameUI.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexGameUI : MonoBehaviour {
+
+	public HexGrid grid;
+
+	HexCell currentCell;
+
+	HexUnit selectedUnit;
+
+	public void SetEditMode (bool toggle) {
+		enabled = !toggle;
+		grid.ShowUI(!toggle);
+		grid.ClearPath();
+	}
+
+	void Update () {
+		if (!EventSystem.current.IsPointerOverGameObject()) {
+			if (Input.GetMouseButtonDown(0)) {
+				DoSelection();
+			}
+			else if (selectedUnit) {
+				if (Input.GetMouseButtonDown(1)) {
+					DoMove();
+				}
+				else {
+					DoPathfinding(selectedUnit.Speed);
+				}
+			}
+		}
+	}
+
+	void DoSelection () {
+		grid.ClearPath();
+		UpdateCurrentCell();
+		if (currentCell) {
+			selectedUnit = currentCell.Unit;
+		}
+	}
+
+	void DoPathfinding (int speed=24) {
+		if (UpdateCurrentCell()) {
+			if (currentCell && selectedUnit.IsValidDestination(currentCell)) {
+				grid.FindPath(selectedUnit.Location, currentCell, speed);
+			}
+			else {
+				grid.ClearPath();
+			}
+		}
+	}
+
+	void DoMove () {
+		if (grid.HasPath) {
+//			selectedUnit.Location = currentCell;
+			selectedUnit.Travel(grid.GetPath());
+			grid.ClearPath();
+		}
+	}
+
+	bool UpdateCurrentCell () {
+		HexCell cell =
+			grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+		if (cell != currentCell) {
+			currentCell = cell;
+			return true;
+		}
+		return false;
+	}
+}
